6869728fc2bd7c350a44eed83389e2f21edb9296
Enoch2k2 <enoch2k2@hotmail.com>
2022-11-03 10:08:10 -0500

    added class scope

--- lib/car.rb
+++ lib/car.rb
@@ -4,6 +4,8 @@ class Car
   # attr_writer :color
   # attr_reader :color
 
+  @@all = []
+  
   def initialize(color:, year:, make:, model:)
     @color = color
     @year = year
@@ -12,16 +14,44 @@ class Car
   end
 
   def start
-    puts "#{ color } car goes VROOM!!!!!"
+    puts "#{ @color } car goes VROOM!!!!!"
   end
 
-  # def color=(color)
-  #   @color = color
-  # end
+  def color
+    @color
+  end
 
-  # def color
-  #   @color
-  # end
+  def save
+    @@all << self
+  end
+
+  def self.create(color:, year:, make:, model:)
+    # # create a new instance
+    car = self.new(color: color, year: year, make: make, model: model)
+    # # save the instance
+    car.save
+    # # return the instance
+    car
+
+    # tap - where we take an object and put it into a block and then returns the object
+
+    # self.new(
+    #   color: color, year: year, make: make, model: model).tap {|obj| obj.save }
+  end
+
+  def self.print_car_models
+    all.each { |car|
+      puts car.model
+    }
+  end  
+
+  def self.all
+    @@all
+  end
 
 end
 
+
+
+# new is unsaved (Car.new)
+# create is saved (Car.create)
